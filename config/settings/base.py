b75032c121af5473f85e35f93a4fcb5e1a4bea4b
tanu268 <tnamdeo09@gmail.com>
2026-07-16 09:17:46 +0530

    added some features

--- config/settings/base.py
+++ config/settings/base.py
@@ -113,7 +113,7 @@ DATABASES = {
 
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 20,
+    "PAGE_SIZE": 500,
     "DEFAULT_FILTER_BACKENDS": [
         "django_filters.rest_framework.DjangoFilterBackend",
         "rest_framework.filters.SearchFilter",
